ebe5478dedf206de1407f098532ccaf667ecbf5a
Alejandro Tejeda <alejanafs28@gmail.com>
2024-09-04 18:55:55 -0600

    Se resolvio el primer ejericcio

--- Practicas/p05/ejercicio.php
+++ Practicas/p05/ejercicio.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejericios</title>
 </head>
 <body>
+    <H1>Respuesta a los ejercicios de la practica</H1>
+    <?php
+    echo "<h3>Ejercicio 1: Variables válidas</h3>";
+    echo "<h3>Variables válidas: \$_myvar, \$_7var, \$myvar, \$var7, \$_element1</h3>";
+    echo "<h3>Variables inválidas: myvar, \$house*5</h3>";
+    echo "<h3>Las variables necesitan empezar con \$ seguidas por un guion bajo (_) o letra, sin caracteres especiales como *</h3>";
     
+    
+    ?>
 </body>
 </html>
