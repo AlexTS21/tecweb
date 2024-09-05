a327fcf70fecdbbeb7edf4c3b36342b72210c094
Alejandro Tejeda <alejanafs28@gmail.com>
2024-09-04 19:28:59 -0600

    Se resolvio el ejericio 3

--- Practicas/p05/ejercicio.php
+++ Practicas/p05/ejercicio.php
@@ -9,9 +9,9 @@
     <H1>Respuesta a los ejercicios de la practica</H1>
     <h2>Ejercicio 1</h2>
     <?php
-        echo "<h3>Variables válidas: \$_myvar, \$_7var, \$myvar, \$var7, \$_element1</h3>";
-        echo "<h3>Variables inválidas: myvar, \$house*5</h3>";
-        echo "<h3>Las variables necesitan empezar con \$ seguidas por un guion bajo (_) o letra, sin caracteres especiales como *</h3>";
+        echo 'Variables válidas: $_myvar, $_7var, $myvar, $var7, $_element1'."<br>";
+        echo 'Variables inválidas: myvar, $house*5'."<br>";
+        echo 'Las variables necesitan empezar con $ seguidas por un guion bajo (_) o letra, sin caracteres especiales como *'."<br>";
     ?>
 
     <h2>Ejericio 2</h2>
@@ -28,6 +28,44 @@
 
         // Imprimir los valores de las variables actualizados
         echo '$a: ' . $a . ' $b: ' . $b . ' $c: ' . $c . "<br>";
+        echo 'Al acer que las vairables $b y $c referencien a $a el contenido de a se copio en las demas variabels'."<br>";
     ?>
+
+    <h2>Ejercicio 3</h2>
+    <?php
+        $a = "PHP5";
+        $z[] = &$a;
+        $b = "5a version de PHP";
+        $c = $b * 10;
+        $a .= $b;
+        $b *= $c;
+        $z[0] = "MySQL";
+
+        echo "Valores de las variables:<br>";
+        echo "\$a = ";
+        var_dump($a);
+        echo "<br>\$b = ";
+        var_dump($b);
+        echo "<br>\$c = ";
+        var_dump($c);
+        echo "<br>\$z = ";
+        print_r($z);
+
+    ?>
+
+    <h2>Ejercicio 4</h2>
+
+    <?php
+        $a = "PHP5";
+        $z[] = &$a;
+        $b = "5a version de PHP";
+        $c = $b * 10;
+
+        echo "Valores usando \$GLOBALS:<br>";
+        echo "\$a = " . $GLOBALS['a'] . "<br>";
+        echo "\$b = " . $GLOBALS['b'] . "<br>";
+        echo "\$c = " . $GLOBALS['c'] . "<br>";
+    ?>
+    <h2>Ejercicio 5</h2>
 </body>
 </html>
