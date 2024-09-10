e38e2abbd4dae2ce59036dce748a6466cf09300d
Alejandro Tejeda <alejanafs28@gmail.com>
2024-09-09 22:32:42 -0600

    Ahora se resive con Post todos los campos del formulario

--- Practicas/p06/concurso.php
+++ Practicas/p06/concurso.php
@@ -5,6 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $phone = htmlspecialchars($_POST['phone']);
     $story = htmlspecialchars($_POST['story']);
+    $color = htmlspecialchars($_POST['color']);
+    $featuresSeleccionadas = $_POST['features']; 
+    $size = htmlspecialchars($_POST['size']);
     
     // Validar que los campos no estén vacíos
     if (!empty($nombre) && !empty($email)) {
@@ -12,6 +15,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Email: " . $email . "<br>";
         echo "Phone: " . $phone . "<br>";
         echo "Story: " . $story . "<br>";
+        echo "Color: " . $color . "<br>";
+        echo "Has seleccionado las siguientes características:<br>";
+        foreach ($featuresSeleccionadas as $feature) {
+            echo htmlspecialchars($feature) . "<br>";
+        }
+        echo "Size: " . $size . "<br>";
     } else {
         echo "Por favor, complete todos los campos.";
     }
