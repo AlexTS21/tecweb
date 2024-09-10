4a1e64de3420d36b7044a2bfd2e5d82323240e75
Alejandro Tejeda <alejanafs28@gmail.com>
2024-09-09 21:49:32 -0600

    Se cambio al metodo POST

--- Practicas/p06/concurso.php
+++ Practicas/p06/concurso.php
@@ -1,7 +1,21 @@
 <?php
-echo $_GET['name'];
-echo $_GET['email'];
-echo $_GET['tel'];
-echo $_GET['feos'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recoger los datos del formulario usando $_POST
+    $nombre = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $story = htmlspecialchars($_POST['story']);
+    
+    // Validar que los campos no estén vacíos
+    if (!empty($nombre) && !empty($email)) {
+        echo "Nombre: " . $nombre . "<br>";
+        echo "Email: " . $email . "<br>";
+        echo "Phone: " . $phone . "<br>";
+        echo "Story: " . $story . "<br>";
+    } else {
+        echo "Por favor, complete todos los campos.";
+    }
+} else {
+    echo "Método de solicitud no válido.";
+}
 ?>
-
