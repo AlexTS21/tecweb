6061a4f93ab5101c593079ec750e322a0107f1bc
Alejandro Tejeda <alejanafs28@gmail.com>
2024-09-12 21:42:48 -0600

    se crearon los archivos necesarios para la practica

---
+++ Practicas/p07/index.php
@@ -0,0 +1,14 @@
+<?php
+    include 'src/funciones.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Practica 7</title>
+</head>
+<body>
+   
+</body>
+</html>
