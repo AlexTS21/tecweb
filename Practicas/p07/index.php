f7f3f4ad9ee779e04b856c0c612e6a545e7d2414
Alejandro Tejeda <alejanafs28@gmail.com>
2024-09-12 21:43:57 -0600

    se resolvio el ejericio 1

--- Practicas/p07/index.php
+++ Practicas/p07/index.php
@@ -9,6 +9,18 @@
     <title>Practica 7</title>
 </head>
 <body>
-   
+     <!-- Comprobar si un número es múltiplo de 5 y 7 -->
+     <h2>1. Múltiplo de 5 y 7</h2>
+    <h3>Agrega tu numero en la url: </h3>
+    <?php
+    if (isset($_GET['numero'])) {
+        $numero = intval($_GET['numero']);
+        if (esMultiploDe5y7($numero)) {
+            echo "<p>$numero es múltiplo de 5 y 7.</p>";
+        } else {
+            echo "<p>$numero no es múltiplo de 5 y 7.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
---
+++ Practicas/p07/src/funciones.php
@@ -0,0 +1,7 @@
+<?php
+    // 1. Función para comprobar si un número es múltiplo de 5 y 7
+    function esMultiploDe5y7($numero) {
+        return ($numero % 5 == 0 && $numero % 7 == 0);
+    }
+
+?>
