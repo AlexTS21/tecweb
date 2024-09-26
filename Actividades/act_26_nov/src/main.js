2097bb474e3bdbdde8883299fb357d22e797e51b
AlexTS21 <alejanafs28@gmail.com>
2024-09-26 14:23:43 -0600

    Se termino el ejercicio 3 y 4

--- Actividades/act_26_nov/src/main.js
+++ Actividades/act_26_nov/src/main.js
@@ -26,3 +26,40 @@ function variablesV(){
     document.write( '<br>' );
     document.write( casado );
 };
+
+function entradaTeclado(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
+
+function suma(){
+    var valor1; 
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número:', '');
+    var suma = parseInt(valor1) + parseInt(valor2);
+    var producto = parseInt(valor1) * parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+
+};
+
+function ifsentence(){
+    var nombre;
+    var nota;
+    nombre = prompt('Ingresa tu nombre:', '');
+    nota = prompt('Ingresa tu nota:', '');
+    if (nota >= 4) {
+        document.write(nombre + ' está aprobado con un ' + nota);
+    }
+};
