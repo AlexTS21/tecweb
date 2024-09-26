5c87bc91bbee3e888bc762e8dd89ef1094408e06
AlexTS21 <alejanafs28@gmail.com>
2024-09-26 14:14:27 -0600

    Se termino el ejercicio 2

--- Actividades/act_26_nov/src/main.js
+++ Actividades/act_26_nov/src/main.js
@@ -11,3 +11,18 @@ function getDatos(){
 function helloWorld(){
     document.write("Hola mundo");
 };
+
+function variablesV(){
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+
+    document.write( nombre );
+    document.write( '<br>' );
+    document.write( edad );
+    document.write( '<br>' );
+    document.write( altura );
+    document.write( '<br>' );
+    document.write( casado );
+};
