9eeb3d316f50414968d074f3154222200d91d58a
Alejandro Tejeda <alejanafs28@gmail.com>
2024-10-08 08:33:46 -0600

    se agrego el boton edit para modificar los datos de la tabla

--- Practicas/p10/get_productos_xhtml_v2.php
+++ Practicas/p10/get_productos_xhtml_v2.php
@@ -3,16 +3,27 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet"
-              href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
-              integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+              href= "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+              integrity= "sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
               crossorigin="anonymous" />
-        <script>
+
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script>    
             function show() {
-                // se obtiene el id de la fila donde está el botón presionado
+                // se obtiene el id de la fila donde está el botón presinado
                 var rowId = event.target.parentNode.parentNode.id;
 
                 // se obtienen los datos de la fila en forma de arreglo
                 var data = document.getElementById(rowId).querySelectorAll(".row-data");
+                /**
+                querySelectorAll() devuelve una lista de elementos (NodeList) que 
+                coinciden con el grupo de selectores CSS indicados.
+                (ver: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors)
+
+                En este caso se obtienen todos los datos de la fila con el id encontrado
+                y que pertenecen a la clase "row-data".
+                */
+
                 var name = data[0].innerHTML;
                 var age = data[1].innerHTML;
 
@@ -20,84 +31,75 @@
 
                 send2form(name, age);
             }
-
-            function save(rowId) {
-                // Obtiene los inputs de la fila
-                var row = document.getElementById(rowId);
-                var newName = row.querySelector(".name-input").value;
-                var newAge = row.querySelector(".age-input").value;
-
-                // Actualiza los valores de la tabla
-                row.querySelector(".name-data").innerHTML = newName;
-                row.querySelector(".age-data").innerHTML = newAge;
-
-                alert("Datos actualizados para: " + newName + ", Edad: " + newAge);
-            }
         </script>
     </head>
     <body>
         
         <table class="table">
-            <thead>
+            <tbody>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Name</th>
                     <th scope="col">Age</th>
-                    <th scope="col">Edit</th>
+                    <th scope=""col>Edit</th>
                     <th scope="col">Submit</th>
+                    
                 </tr>
-            </thead>
-            <tbody>
+
                 <tr id="1">
                     <th scope="row">1</th>
-                    <td class="name-data">Sara</td>
-                    <td class="age-data">23</td>
-                    <td>
-                        <input type="text" class="name-input" value="Sara" />
-                        <input type="text" class="age-input" value="23" />
-                    </td>
-                    <td>
-                        <input type="button" value="submit" onclick="show()" />
-                        <input type="button" value="save" onclick="save('1')" />
-                    </td>
+                    <td class="row-data">Sara</td>
+                    <td class="row-data">23</td>
+                    <td><input type="button" value="edit" onclick="edit()"/></td>
+                    <td><input type="button" 
+                               value="submit" 
+                               onclick="show()" /></td>
                 </tr>
 
                 <tr id="2">
                     <th scope="row">2</th>
-                    <td class="name-data">John</td>
-                    <td class="age-data">30</td>
-                    <td>
-                        <input type="text" class="name-input" value="John" />
-                        <input type="text" class="age-input" value="30" />
-                    </td>
-                    <td>
-                        <input type="button" value="submit" onclick="show()" />
-                        <input type="button" value="save" onclick="save('2')" />
-                    </td>
+                    <td class="row-data">John</td>
+                    <td class="row-data">30</td>
+                    <td><input type="button" value="edit" onclick="edit()"/></td>
+                    <td><input type="button" 
+                               value="submit" 
+                               onclick="show()" /></td>
                 </tr>
 
                 <tr id="3">
                     <th scope="row">3</th>
-                    <td class="name-data">Naman</td>
-                    <td class="age-data">20</td>
-                    <td>
-                        <input type="text" class="name-input" value="Naman" />
-                        <input type="text" class="age-input" value="20" />
-                    </td>
-                    <td>
-                        <input type="button" value="submit" onclick="show()" />
-                        <input type="button" value="save" onclick="save('3')" />
-                    </td>
+                    <td class="row-data">Naman</td>
+                    <td class="row-data">20</td>
+                    <td><input type="button" value="edit" onclick="edit()"/></td>
+                    <td><input type="button" 
+                               value="submit" 
+                               onclick="show()" /></td>
                 </tr>
             </tbody>
         </table>
-
         <script>
-            function send2form(name, age) {     
+            function send2form(name, age) {     //form) { 
                 var urlForm = "http://localhost/tecweb/Practicas/p10/formulario.php";
-                var propName = "nombre=" + name;
-                var propAge = "edad=" + age;
-                window.open(urlForm + "?" + propName + "&" + propAge);
+                var propName = "nombre="+name;
+                var propAge = "edad="+age;
+                window.open(urlForm+"?"+propName+"&"+propAge);
+            }
+
+            function edit(){
+                let newName = prompt("Ingresa el nuevo nombre");
+                let newAge = prompt("Ingresa la nueva edad");
+                // se obtiene el id de la fila donde está el botón presinado
+                var rowId = event.target.parentNode.parentNode.id;
+                let row =  document.getElementById(rowId)
+
+                // Seleccionar las celdas con clase "row-data" en la fila
+                let cells = row.getElementsByClassName('row-data');
+
+               
+                
+                // Actualizar los valores de las celdas
+                cells[0].innerText = newName;  // Actualizar nombre
+                cells[1].innerText = newAge;   // Actualizar edad
             }
         </script>
     </body>
