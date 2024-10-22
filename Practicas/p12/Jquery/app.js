e436519d3d45f7a51f2fb3e60e9ecd04ee36de91
Alejandro Tejeda <alejanafs28@gmail.com>
2024-10-21 20:30:22 -0600

    Se agrego la funcion de agregar producto

--- Practicas/p12/Jquery/app.js
+++ Practicas/p12/Jquery/app.js
@@ -27,18 +27,68 @@ $(document).ready(function(){
             type: 'POST',
             data: {search},
             success: function(response){
-                console.log(response)
-                let tasks = JSON.parse(response);
-                    let template = '';
-                    tasks.forEach(task => {
-                        template += `<li>
-                            ${task.nombre}
-                        </li>`
-                    });
+                //console.log(response)
+                let productos = JSON.parse(response);
+                // SE VERIFICA SI EL OBJETO JSON TIENE DATOS
+            if(Object.keys(productos).length > 0) {
+                // SE CREA UNA PLANTILLA PARA CREAR LAS FILAS A INSERTAR EN EL DOCUMENTO HTML
+                let template = '';
+                let template_bar = '';
 
-                    $('#container').html(template);
-                    $('#product-result').show();
+                productos.forEach(producto => {
+                    // SE COMPRUEBA QUE SE OBTIENE UN OBJETO POR ITERACIÓN
+                    //console.log(producto);
+
+                    // SE CREA UNA LISTA HTML CON LA DESCRIPCIÓN DEL PRODUCTO
+                    let descripcion = '';
+                    descripcion += '<li>precio: '+producto.precio+'</li>';
+                    descripcion += '<li>unidades: '+producto.unidades+'</li>';
+                    descripcion += '<li>modelo: '+producto.modelo+'</li>';
+                    descripcion += '<li>marca: '+producto.marca+'</li>';
+                    descripcion += '<li>detalles: '+producto.detalles+'</li>';
+                
+                    template += `
+                        <tr productId="${producto.id}">
+                            <td>${producto.id}</td>
+                            <td>${producto.nombre}</td>
+                            <td><ul>${descripcion}</ul></td>
+                            <td>
+                                <button class="product-delete btn btn-danger" onclick="eliminarProducto()">
+                                    Eliminar
+                                </button>
+                            </td>
+                        </tr>
+                    `;
+
+                    template_bar += `
+                        <li>${producto.nombre}</il>
+                    `;
+                    });
+                    // SE HACE VISIBLE LA BARRA DE ESTADO
+                    document.getElementById("product-result").className = "card my-4 d-block";
+                    // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
+                    document.getElementById("container").innerHTML = template_bar;  
+                    // SE INSERTA LA PLANTILLA EN EL ELEMENTO CON ID "productos"
+                    document.getElementById("products").innerHTML = template;
+                }
             }
         })
+    })
+
+    $('#product-form').submit(function(e){
+        
+        
+        
+        let descripcion =  $('#description').val();
+
+        let obj =  JSON.parse(descripcion);
+        obj.nombre =  $('#name').val();
+
+        const producto = JSON.stringify(obj)
+        console.log(producto)
+        $.post('backend/product-add.php', producto, function(response){
+            console.log(response)
+        })
+        e.preventDefault();
     })
 });
